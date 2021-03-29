7fd0b067e5b0d29f9489dc23c6dea11292d49bb5
Irene <irene1martin2menendez@gmail.com>
2021-03-29 11:58:33 +0100

    New updated responsive formats

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,19 @@ import logo from './Logos/8.png';
 
 export function Header() {
     return (
-        <header className="header">
-            <div className="container">
-            <TypeWriterEffect className="animation"
-                              textStyle={{fontFamily: 'Montserrat',}}
-                              startDelay={100}
-                              cursorColor="white"
-                              text="Irene Martin. Web Developer."
-                              typeSpeed={100}
-            />
-            <p className="pHeader">Creation without Limits.</p>
-        </div>
+        <header>
+            <div className="header">
+                <div className="banner">
+                    <TypeWriterEffect className="animation"
+                                      textStyle={{fontFamily: 'Montserrat',}}
+                                      startDelay={100}
+                                      cursorColor="white"
+                                      text="Irene Martin. Web Developer."
+                                      typeSpeed={100}
+                    />
+                    <p className="pHeader">Creation without Limits.</p>
+                </div>
+            </div>
         </header>
     );
 }
